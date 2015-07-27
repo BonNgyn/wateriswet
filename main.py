51dc82711a779b6cea6101e5fbc9e505cd801d93
Irwin Dominguez <irwind500@gmail.com>
2015-07-27 10:31:54 -0700

    you can now edit the html file   instead of the main.py one

--- main.py
+++ main.py
@@ -15,10 +15,16 @@
 # limitations under the License.
 #
 import webapp2
+import jinja2
+import os
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('WATER..... IS WET - Truth')
+        template = jinja_environment.get_template('templates/main.html')
+        self.response.write(template.render({}))
+        
+jinja_environment = jinja2.Environment(loader=
+jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
