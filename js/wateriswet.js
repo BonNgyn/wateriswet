e614c6fcd446ea707d5118c1b3a5f08dafa05631
lillymliu <demouser@MacBook-Air.local>
2015-07-29 09:41:07 -0700

    supplies works! kinda

--- js/wateriswet.js
+++ js/wateriswet.js
@@ -6,7 +6,6 @@ window.onload = function()
 
     ctx.lineWidth = 2; // Our border will have a thickness of 2 pixels
     ctx.strokeStyle = 'black'; // The border will also be black
-
     randx = Math.floor((Math.random() * (canvas.width - 100)) + 50);
     randy = Math.floor((Math.random() * (canvas.height - 100)) + 50);
     dx = randx;
@@ -16,16 +15,6 @@ window.onload = function()
 
     randyellowx = Math.floor((Math.random() * (canvas.width - 100)) + 50);
     randyellowy = Math.floor((Math.random() * (canvas.height - 100)) + 50);
-
-
-    dx = Math.floor((Math.random() * (canvas.width - 80)) + 50);
-    dy = Math.floor((Math.random() * (canvas.height - 80)) + 50);
-    x = 0;
-    y = 0;
-
-    randyellowx = Math.floor((Math.random() * (canvas.width - 80)) + 50);
-    randyellowy = Math.floor((Math.random() * (canvas.height - 80)) + 50);
-
 
     randredx = Math.floor((Math.random() * (canvas.width - 100)) + 50);
     randredy = Math.floor((Math.random() * (canvas.height - 100)) + 50);
@@ -39,127 +28,92 @@ window.onload = function()
     yellow_supplies();
     red_triangle();
 
-    //steps goes down each time an arrow key is pressed
+
+
     var steps;
     steps = 100;
-    $( "#scorenum").text(steps);
+    $( "#scorenum" ).text(steps);
 
-
-    var currentKey;          //records the current key pressed
-    var TimerWalk;          //timer handle
-    var charWalk = 2;       //1=1st foot, 2=stand, 3=2nd foot, 4=stand
-    var charSpeed = 400;
-    $(document).ready(function() {
-
-     //add character state class
-     $('#bloo').addClass('front-stand');
-
- });
-
- }
-
-
-
-    $(document).keydown(function(e) {
-
-      steps = steps -1;
-      $( "#scorenum" ).text(steps);
-
-     console.log(steps);
-     if (steps <0){
-       console.log('gameover');
-       window.location.href = "/end";
-        }
-    });
-
-//functions or the 3 characters
     var currentKey;          //records the current key pressed
     var TimerWalk;          //timer handle
     var charStep = 2;       //1=1st foot, 2=stand, 3=2nd foot, 4=stand
     var charSpeed = 400;
     $(document).ready(function() {
 
-
      //add character state class
      $('#bloo').addClass('front-stand');
 
      });
 
+    $(document).keydown(function(e) {
 
-function blue_bloo() {
-    bloo_image = new Image();
-    bloo_image.src = 'https://wiki.guildwars2.com/images/8/8c/Blue_Dot.png';
-    bloo_image.onload = function() {
-        ctx.drawImage(bloo_image, dy+y,dx+x)
-    }
-}
+      steps = steps -1;
+      $( "#scorenum" ).text(steps);
+      if (!currentKey) {
 
-function yellow_supplies() {
-    yellow_image = new Image();
-    yellow_image.src = 'http://www.bodenimages.com/productimages/sw/15GAUT_33375_YEL_s.jpg';
-    yellow_image.onload = function() {
-        ctx.drawImage(yellow_image, randyellowy, randyellowx)
-    }
-}
+       //set the currentKey to the key that is down
+       currentKey = e.keyCode;
 
-function doKeyDown(e) {
-    if((dx+x)<(canvas.height-40)){
-        if(e.keyCode == 40) /*down*/{
-            clearCanvas();
-            drawMain();
-            yellow_supplies();
-            x = x + 10;
+       //execute character movement function charWalk('direction')
+        switch(e.keyCode) {
+         case 38: charWalk('up');    break;
+         case 39: charWalk('right'); break;
+         case 40: charWalk('down');  break;
+         case 37: charWalk('left');  break;
+       }
+     }
+
+     console.log(steps);
+     if (steps <=0){
+       console.log('gameover');
+       window.location.href = "/end";
+        }
+      blue_yellow_collision();
+    });
+
+    function blue_bloo() {
+        bloo_image = new Image();
+        bloo_image.src = 'https://wiki.guildwars2.com/images/8/8c/Blue_Dot.png';
+        bloo_image.onload = function() {
             ctx.drawImage(bloo_image, dy+y,dx+x)
         }
     }
-    if((dy+y)>(0)){
-        if(e.keyCode == 37) /*left*/{
-            clearCanvas();
-            drawMain();
-            yellow_supplies();
-            y = y - 10;
-            ctx.drawImage(bloo_image, dy+y,dx+x)
-        }
-    }
-    if((dy+y)<(canvas.width-40)){
-        if(e.keyCode == 39) /*right*/{
-            clearCanvas();
-            drawMain();
-            yellow_supplies();
-            y = y + 10;
-            ctx.drawImage(bloo_image, dy+y,dx+x)
-        }
-    }
-    if((dx+x)>(20)){
-        if(e.keyCode == 38) /*up*/{
-            clearCanvas();
-            drawMain();
-            yellow_supplies();
-            x = x - 10;
-            ctx.drawImage(bloo_image, dy+y,dx+x)
 
+    function yellow_supplies() {
+        yellow_image = new Image();
+        yellow_image.src = 'http://www.bodenimages.com/productimages/sw/15GAUT_33375_YEL_s.jpg';
+        yellow_image.onload = function() {
+            ctx.drawImage(yellow_image, randyellowy, randyellowx)
+        }
+    }
 
     function red_triangle() {
-        red_triangle = new Image();
-        red_triangle.src = 'images/red_triangle.png';
+        var red_triangle = new Image();
+        red_triangle.src = '/images/red_triangle.png';
         red_triangle.onload = function() {
             ctx.drawImage(red_triangle, randredy, randredx)
         }
     }
-//Function for Bloo to move
 
-    function checkSupplies() {
-        if((randyellowx>(dx+x)>(randyellowx-10)) || ((randyellowy)>(dy+y)>(randyellowy-10)) ||
-            (randyellowx>(dx+x)>(randyellowx+10)) || ((randyellowy)>(dy+y)>(randyellowy+10)) ||
-            ((randyellowx+10)>(dx+x)>(randyellowx)) || ((randyellowy+10)>(dy+y)>(randyellowy)) ||
-            ((randyellowx-10)>(dx+x)>(randyellowx)) || ((randyellowy-10)>(dy+y)>(randyellowy))){
-            steps = steps + 10;
-            randyellowx = Math.floor((Math.random() * (canvas.width - 80)) + 50);
-            randyellowy = Math.floor((Math.random() * (canvas.height - 80)) + 50);
-            yellow_supplies();
+    function blue_yellow_collision(){
+      console.log("blue coords:" + dx + x + "," + dy +y);
+      console.log("yellow coods: " + randyellowx + "," +randyellowy);
+      if ( ((dx + x )< randyellowx) && (randyellowx < (dx + x + 40) ) ) {
+          if( ((dy +y ) <randyellowy) && (randyellowy< (dy + y + 40)) ){
+          console.log("collided");
+          steps = steps + 10;
         }
-    }
+      }
+      if ( (randyellowx < (dx + x) )&& (dx < (randyellowx + y +40) )){
+        if ((randyellowy < (dy+ y)) &&  (dy < (randyellowy + y +40))){
+          console.log("collideded");
+          steps = steps + 10;
+        }
+      }
+ red_triangle();
 
+
+    }
 
     function doKeyDown(e) {
         if((dx+x)<(canvas.height-40)){
@@ -167,7 +121,6 @@ function doKeyDown(e) {
                 clearCanvas();
                 drawMain();
                 yellow_supplies();
-                checkSupplies();
                 x = x + 10;
                 ctx.drawImage(bloo_image, dy+y,dx+x)
             }
@@ -177,7 +130,6 @@ function doKeyDown(e) {
                 clearCanvas();
                 drawMain();
                 yellow_supplies();
-                checkSupplies();
                 y = y - 10;
                 ctx.drawImage(bloo_image, dy+y,dx+x)
             }
@@ -187,7 +139,6 @@ function doKeyDown(e) {
                 clearCanvas();
                 drawMain();
                 yellow_supplies();
-                checkSupplies();
                 y = y + 10;
                 ctx.drawImage(bloo_image, dy+y,dx+x)
             }
@@ -197,59 +148,18 @@ function doKeyDown(e) {
                 clearCanvas();
                 drawMain();
                 yellow_supplies();
-                checkSupplies();
                 x = x - 10;
                 ctx.drawImage(bloo_image, dy+y,dx+x)
-
             }
-
-
         }
+
     }
 
-}
-
-function clearCanvas() {
-    canvas.width = canvas.width;
-}
-
-
-function drawMain()
-  {
-      ctx.lineWidth = 2; // Our border will have a thickness of 2 pixels
-      ctx.strokeStyle = 'black'; // The border will also be black
-
-      // The border is drawn on the outside of the rectangle, so we'll
-      // need to move it a bit to the right and up. Also, we'll need
-      // to leave a 20 pixels space on the top to draw the interface.
-
-      ctx.strokeRect(2, 40, canvas.width - 4, canvas.height - 40);
-      ctx.font = '30px sans-serif';
-      // ctx.fillText('Steps: ' + steps, 2, 30);
-  }
-}
-
-        }
-      }
-
-
-//When the characters collide
-        function isCollide(a, b) {
-    return !(
-        ((blue_bloo.y + blue_bloo.height) < (yellow_supplies.y)) ||
-        (blue_bloo.y > (yellow_supplies.y + yellow_supplies.height)) ||
-        ((blue_bloo.x + blue_bloo.width) < yellow_supplies.x) ||
-        (blue_bloo.x > (yellow_supplies.x + yellow_supplies.width))
-    );
-
-  }
-
-//the canvas clears
     function clearCanvas() {
         canvas.width = canvas.width;
     }
 
-//draws the borders
+
     function drawMain()
         {
             ctx.lineWidth = 2; // Our border will have a thickness of 2 pixels
