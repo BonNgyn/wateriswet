c7ac8e687745576dab1c095cf942aee77ef53a30
Bonnie Nguyen <bonngyn@D-10-154-63-14.dhcp4.washington.edu>
2015-07-30 21:55:54 -0700

    MERGED! with CSS, everything working.

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ from google.appengine.ext import ndb
 import logging
 import json
 
+player_ancestor = ndb.Key('Player', "high_score")
+
 class Player(ndb.Model):
     name = ndb.StringProperty(required=True) 
-    collectednum = ndb.StringProperty(required=True)
-    date_created = ndb.DateTimeProperty(auto_now_add=True)      
+    collectednum = ndb.IntegerProperty(required=True)
+    date_created = ndb.DateProperty(auto_now_add=True)      
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
@@ -20,20 +22,18 @@ class MainHandler(webapp2.RequestHandler):
 class GameHandler(webapp2.RequestHandler):
     def get(self):
         template = jinja_environment.get_template('templates/game.html')
-        collectednum = self.request.get('collectednum')
-        template_vars = {'collectednum': collectednum}
-        self.response.write(template.render((template_vars)))  
+        self.response.write(template.render())  
 
 class CreatePlayer(webapp2.RequestHandler):
     def post(self):
         name = self.request.get('player')
         collectednum = self.request.get('collectednum')
+        collected = int(collectednum)
         date = datetime.datetime.now()
-        player = Player(name=name, collectednum = collectednum)
+        player = Player(name=name, collectednum = collected, parent = player_ancestor)
         player.date_created = date
         player.put()
-        template = jinja_environment.get_template('templates/highscore.html')
-        self.response.write(template.render())
+        self.redirect('/highscore')
 
 
 class EndHandler(webapp2.RequestHandler):
@@ -45,8 +45,12 @@ class EndHandler(webapp2.RequestHandler):
 
 class HighScoreHandler(webapp2.RequestHandler):
     def get(self):
+        query = Player.query(ancestor=player_ancestor)
+        player_data = query.fetch()
+        player_data_sorted = sorted(player_data, key=lambda player: -player.collectednum)
+        template_vars = {'players':player_data_sorted}
         template = jinja_environment.get_template('templates/highscore.html')
-        self.response.write(template.render())
+        self.response.write(template.render(template_vars))
 
 
 jinja_environment = jinja2.Environment(loader=
